8535fe448340d195cdff920b080902d7ef75b6c1
VENKATARAMANA BARATAM <109843726+Venkataramanabaratam1@users.noreply.github.com>
2025-04-06 16:42:35 +0530

    Update app.js

--- HospitalManagementSystem/backend/app.js
+++ HospitalManagementSystem/backend/app.js
@@ -12,7 +12,7 @@ import appointmentRouter from "./router/appointmentRouter.js";
 const app = express();
 config({ path: "./config.env" });
 
-app.use('/', (req, res) => {
+app.get('/', (req, res) => {
   res.status(200).json({ message: 'Backend is running' });
 });
 
