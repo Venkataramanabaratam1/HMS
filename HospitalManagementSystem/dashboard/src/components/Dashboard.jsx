7ce985caf50e1a1482bb8df8653afc3aa3ff9226
VENKATARAMANA BARATAM <109843726+Venkataramanabaratam1@users.noreply.github.com>
2025-04-06 18:02:16 +0530

    Update Dashboard.jsx

--- HospitalManagementSystem/dashboard/src/components/Dashboard.jsx
+++ HospitalManagementSystem/dashboard/src/components/Dashboard.jsx
@@ -13,8 +13,7 @@ const Dashboard = () => {
     const fetchAppointments = async () => {
       try {
         const { data } = await axios.get(
-          "http://localhost:4000/api/v1/appointment/getall",
-          { withCredentials: true }
+           import.meta.env.VITE_APP_BACKEND+'api/v1/appointment/getall'
         );
         setAppointments(data.appointments);
       } catch (error) {
